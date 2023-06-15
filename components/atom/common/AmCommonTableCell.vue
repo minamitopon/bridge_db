d8d47ad07e9eff8147118c0e4f318290d61b779b
minamino shiho <shelling519@gmail.com>
2023-06-16 01:10:48 +0900

    fix table

---
+++ components/atom/common/AmCommonTableCell.vue
@@ -0,0 +1,12 @@
+<script lang="ts" setup></script>
+
+<template lang="pug">
+td.am-common-table-cell
+  slot
+</template>
+
+<style lang="sass">
+.am-common-table-cell
+  border: solid 2px #646464
+  padding: 4px 8px
+</style>
