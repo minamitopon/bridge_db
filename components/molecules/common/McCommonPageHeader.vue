e9d9fed52fd5301934a5d272281c1c290b3816e7
minamino shiho <shelling519@gmail.com>
2024-03-07 00:43:07 +0900

    add /match

---
+++ components/molecules/common/McCommonPageHeader.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+/** interface */
+interface Props {
+  title: string;
+}
+interface Emits {
+  (e: "back"): void;
+}
+/** defines */
+const Props = withDefaults(defineProps<Props>(), {});
+const emits = defineEmits<Emits>();
+
+/** emits */
+const emitBack = () => {
+  emits("back");
+};
+</script>
+
+<template lang="pug">
+.page-header
+  el-page-header(@back="emitBack")
+    template(#content)
+      span.page-header-title
+        | {{ title }}
+</template>
---
+++ pages/match/[uuid].vue
@@ -0,0 +1,28 @@
+<script lang="ts" setup>
+import { ref, computed, onMounted } from "vue";
+/** defines */
+interface Props {}
+/** defines */
+const Props = withDefaults(defineProps<Props>(), {});
+const route = useRoute();
+const uuid = ref(route.params.uuid);
+const handleBack = () => {
+  /* NOP */
+};
+
+const vugraphData = computed(() => {
+  console.log(uuid);
+  return;
+});
+onMounted(() => {
+  // Promise.all();
+});
+</script>
+
+<template lang="pug">
+.match
+  .match-header
+    mc-common-page-header(title="test" @back="handleBack")
+  .match-main
+    p {{ uuid }}
+</template>
